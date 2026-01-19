e9272f1f71272fd2e74e25561e63c7ab5940c1d1
Andrew Dutka <andrewadutka@gmail.com>
2026-01-19 19:15:22 -0400

    refactored to use crossterm interfaces

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
+use crossterm::event::{Event, KeyCode, KeyModifiers};
 use crossterm::terminal::{self, Clear};
 use crossterm::{cursor::MoveTo, execute, queue, style::Print};
-use std::io::{self, Read, Stdout, Write};
+use std::io::{self, Stdout, Write};
 
 fn clear_screen(stdout: &mut Stdout) -> io::Result<()> {
     execute!(stdout, terminal::Clear(terminal::ClearType::All),)?;
@@ -16,12 +17,11 @@ fn insert_tildes(stdout: &mut Stdout) -> io::Result<()> {
 }
 
 fn print_name(stdout: &mut Stdout) -> io::Result<()> {
-    let name = "rw v0.0.0";
+    let name = "redwrite v0.0.0 (idk i can't think of a name)";
     let name_x_pos = terminal::size()?.0 / 2 - name.len() as u16 / 2;
     let name_y_pos = terminal::size()?.1 - terminal::size()?.1 / 3;
 
     execute!(stdout, MoveTo(name_x_pos, name_y_pos), Print(name))?;
-
     Ok(())
 }
 
@@ -34,40 +34,53 @@ fn editor_init(stdout: &mut Stdout) -> io::Result<()> {
     Ok(())
 }
 
-fn editor_end() -> io::Result<()> {
+fn editor_end(stdout: &mut Stdout) -> io::Result<()> {
     terminal::disable_raw_mode()?;
+    clear_screen(stdout)?;
     Ok(())
 }
 
 fn main() -> io::Result<()> {
-    let stdin = io::stdin(); // create input stream handler
     let mut stdout = io::stdout(); // create output stream handler
-    let input = stdin.bytes(); // read input bytes
 
     editor_init(&mut stdout)?;
 
     let mut arr: Vec<char> = Vec::new(); // store the characters on the line
 
-    for character in input {
-        let b = character.unwrap();
-        let c = b as char;
+    loop {
+        let event = match crossterm::event::read() {
+            Ok(e) => e,
+            Err(_) => {
+                dbg!("read was not successful.");
+                break;
+            }
+        };
 
-        if b == 3 {
-            editor_end()?;
-            break;
-        } else if b == 127 {
-            arr.pop();
-        } else {
-            arr.push(c);
+        if let Event::Key(key_event) = event {
+            match key_event.code {
+                KeyCode::Backspace => {
+                    arr.pop();
+                }
+                KeyCode::Char('c') => {
+                    if key_event.modifiers == KeyModifiers::CONTROL {
+                        editor_end(&mut stdout)?;
+                        break;
+                    }
+                }
+                KeyCode::Char(some) => {
+                    arr.push(some);
+                }
+                _ => (),
+            }
         }
 
-        execute!(
+        queue!(
             stdout,
             Print("\r"),
             Clear(terminal::ClearType::UntilNewLine)
         )?;
-        for thing in &arr {
-            queue!(stdout, Print(thing))?;
+        for key in &arr {
+            queue!(stdout, Print(key))?;
         }
         stdout.flush()?;
     }
